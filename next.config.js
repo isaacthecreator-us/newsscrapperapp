b987ca5e8584de842695a03bbf6c88b5f6195367
isaacthecreator-us <admin@isaacthecreator.com>
2026-01-28 21:34:20 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['www.google.com', 'encrypted-tbn0.gstatic.com'],
-  },
-}
-
-module.exports = nextConfig
